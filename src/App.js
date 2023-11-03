2528639e732cb4afa7dd460e2902c05b9038399c
Sushank Dhungana <dhunganasushank3@gmail.com>
2023-11-03 21:29:36 +0545

    (3rd commit): Added functionality of footer like calculation the total items, packed items and the percentage of the items that have been passed

--- src/App.js
+++ src/App.js
@@ -119,12 +119,20 @@ function Item({ item, onDeleteItems, onUpdateItems }) {
 }
 
 function Stats({ items }) {
+  const total = items.length;
+  const packed = items.filter((item) => item.packed === true).length;
+  const perc = Math.round((packed / total) * 100);
+
   return (
     <footer className="stats">
-      <em>
-        💼 You have {items.length} {items.length >= 2 ? "items" : "item"} on
-        your list and you already passed X(X%)
-      </em>
+      {total > 0 ? (
+        <em>
+          💼 You have {total} {items.length >= 2 ? "items" : "item"} on your
+          list and you already packed {packed + " "}({perc}%)
+        </em>
+      ) : (
+        <em>Start adding some items to your packing list 🚀</em>
+      )}
       <p>
         &copy; {new Date().getFullYear()} Jonas Schmedtmann(From the React
         course), Programmer:{" "}
